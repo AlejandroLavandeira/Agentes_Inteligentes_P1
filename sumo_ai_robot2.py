24c7589e61fdae58c5006c505299b2aad06a4e37
AlejandroLavandeira <alejandro.lavandeira@rai.usc.es>
2025-10-07 13:49:11 +0200

    Update sumo_ai_robot2.py
    Temporizador 4 segundos robot2

--- sumo_ai_robot2.py
+++ sumo_ai_robot2.py
@@ -37,6 +37,14 @@ class SumoAI:
         rospy.Subscriber(self.ns + '/scan', LaserScan, self.callback_laser)
         rospy.Subscriber(self.ns + '/odom', Odometry, self.callback_odometria)
 
+        rospy.loginfo("Esperando 4 segundos para que Gazebo inicie...")
+        try:
+            rospy.sleep(4) # Pausa de 4 segundos sincronizada con el reloj de ROS/Gazebo
+        except rospy.ROSInterruptException:
+            rospy.logwarn("Pausa interrumpida por cierre del nodo.")
+            return # Salimos si el nodo se cierra durante la pausa
+        rospy.loginfo("¡Iniciando IA!")
+        
         # --- 5. El "Corazon" de la IA ---
         rospy.Timer(rospy.Duration(0.1), self.ejecutar_ciclo)
 
